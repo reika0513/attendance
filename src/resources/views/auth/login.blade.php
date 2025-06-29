93b00a4bddb4629bdcbd534aa639b91d53c433bd
reika <r.okinu.513@gmail.com>
2025-06-30 07:39:41 +0900

    ログイン画面 view

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,10 +1,46 @@
 @extends('layouts.app_auth')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 @endsection
 
 @section('content')
-
-
+<div class="login">
+    <div class="login_header">
+        <h1 class="login_header-logo">ログイン</h1>
+    </div>
+    <form class="form" action="/login" method="post">
+        @csrf
+        <div class="form_frame">
+            <div class="form_group">
+                <p class="form_title">メールアドレス</p>
+                <div class="form_group-content">
+                    <div class="form_group-text">
+                        <input class="form_group-text_input" name="email" type="email" value="{{ old('email')}}">
+                    </div>
+                    <div class="form_error">
+                        <!--エラーメッセージ-->
+                    </div>
+                </div>
+            </div>
+            <div class="form_group">
+                <p class="form_title">パスワード</p>
+                <div class="form_group-content">
+                    <div class="form_group-text">
+                        <input class="form_group-text_input" name="password" type="text">
+                    </div>
+                    <div class="form_error">
+                        <!--エラーメッセージ-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form_button">
+            <button class="form_button-submit" type="submit">ログインする</button>
+        </div>
+    </form>
+    <div class="login">
+        <a class="login_button" href="/register">会員登録はこちら</a>
+    </div>
+</div>
 @endsection
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,10 +1,69 @@
 @extends('layouts.app_auth')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
 @endsection
 
 @section('content')
-
+<div class="register">
+    <div class="register_header">
+        <h1 class="register_header-logo">会員登録</h1>
+    </div>
+    <form class="form" action="/register" method="post">
+        @csrf
+        <div class="form_frame">
+            <div class="form_group">
+                <p class="form_title">名前</p>
+                <div class="form_group-content">
+                    <div class="form_group-text">
+                        <input class="form_group-text_input" name="name" type="text" value="{{ old('name')}}">
+                    </div>
+                    <div class="form_error">
+                        <!--エラーメッセージ-->
+                    </div>
+                </div>
+            </div>
+            <div class="form_group">
+                <p class="form_title">メールアドレス</p>
+                <div class="form_group-content">
+                    <div class="form_group-text">
+                        <input class="form_group-text_input" name="email" type="email" value="{{ old('email')}}">
+                    </div>
+                    <div class="form_error">
+                        <!--エラーメッセージ-->
+                    </div>
+                </div>
+            </div>
+            <div class="form_group">
+                <p class="form_title">パスワード</p>
+                <div class="form_group-content">
+                    <div class="form_group-text">
+                        <input class="form_group-text_input" name="password" type="password">
+                    </div>
+                    <div class="form_error">
+                        <!--エラーメッセージ-->
+                    </div>
+                </div>
+            </div>
+            <div class="form_group">
+                <p class="form_title">パスワード確認</p>
+                <div class="form_group-content">
+                    <div class="form_group-text">
+                        <input class="form_group-text_input" name="password_confirmation" type="password">
+                    </div>
+                    <div class="form_error">
+                        <!--エラーメッセージ-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form_button">
+            <button class="form_button-submit" type="submit">登録する</button>
+        </div>
+    </form>
+    <div class="login">
+        <a class="login_button" href="/login">ログインはこちら</a>
+    </div>
+</div>
 
 @endsection
