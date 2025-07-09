e86af3a946ee741160da5bb2a9386c5925a69632
reika <r.okinu.513@gmail.com>
2025-07-09 20:15:20 +0900

    時計　view

--- src/resources/views/stamp.blade.php
+++ src/resources/views/stamp.blade.php
@@ -10,11 +10,9 @@
         <div class="status">
             <p class="status_icon">出勤</p>
         </div>
-        <div class="date">
-            <p class="date_icon">2023年6月1日(木)</p>
-        </div>
         <div class="clock">
-            <p class="clock_icon">8:00</p>
+            <p class="clock_date" id="clock-date"></p>
+            <p class="clock_time" id="clock-time"></p>
         </div>
         <div class="stamp_button">
             <!-- if関数?　それぞれ表示を変える -->
@@ -25,4 +23,44 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    function twoDigit(num) {
+      var ret;
+      if( num < 10 ) {ret = "0" + num; }
+      else {ret = num;} 
+      return ret;
+    }
+
+    function showDate() {
+        var nowDate = new Date();
+        var Year = nowDate.getFullYear();
+        var month  = nowDate.getMonth()+1;
+        var week = nowDate.getDay();
+        var day = nowDate.getDate();
+        var week_ja= new Array("日","月","火","水","木","金","土");
+        var week_ja=week_ja[week];
+        var msg = Year + "年" + month + "月" + day + "日" + week_ja +"曜日";
+        document.getElementById("clock-date").innerHTML = msg;
+    }
+    setInterval('showDate()',1000);
+</script>
+
+<script type="text/javascript">
+    function twoDigit(num) {
+      var ret;
+      if( num < 10 ) {ret = "0" + num; }
+      else {ret = num;} 
+      return ret;
+    }
+
+    function showClock() {
+        var nowTime = new Date();
+        var nowHour = nowTime.getHours();
+        var nowMin  = nowTime.getMinutes();
+        var msg = nowHour + ":" + nowMin;
+        document.getElementById("clock-time").innerHTML = msg;
+    }
+    setInterval('showClock()',1000);
+</script>
 @endsection
