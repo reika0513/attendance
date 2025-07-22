0a2f0d7fe7ee5f116389f22cad94cd5ccb8a9516
reika <r.okinu.513@gmail.com>
2025-07-23 07:17:38 +0900

    休憩打刻機能　追加

--- src/app/Models/Timestamp.php
+++ src/app/Models/Timestamp.php
@@ -20,4 +20,9 @@ class Timestamp extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function rest()
+    {
+        return $this->hasOne(Rest::class);
+    }
+
 }
---
+++ src/database/migrations/2025_07_23_071144_create_rests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rests', function (Blueprint $table) {
+            $table->id();
+            $table->integer('timestamp_id')->unsigned();
+            $table->datetime('rest_in');
+            $table->datetime('rest_out')->nullable();
+            $table->timestamps();
+
+            $table->foreign('timestamp_id')->references('id')->on('timestamps');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rests');
+    }
+}
